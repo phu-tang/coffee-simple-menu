41fcbfc16466f68d1c60acabd793c7619bfcd7d4
Phu Tang <phu.tang@tiki.vn>
2020-02-22 00:40:39 +0700

    Updates

---
+++ precache-manifest.e935fef34b642521cc2363008a78981e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544ca06262931cd28efbbd5461d0c406",
+    "url": "/coffee-simple-menu/index.html"
+  },
+  {
+    "revision": "2e6387e82c3e15844f7d",
+    "url": "/coffee-simple-menu/static/css/main.0b35c1b8.chunk.css"
+  },
+  {
+    "revision": "f84a2656049266783426",
+    "url": "/coffee-simple-menu/static/js/2.83fe0153.chunk.js"
+  },
+  {
+    "revision": "e5dfee980da33f2036bc0f2f533a1020",
+    "url": "/coffee-simple-menu/static/js/2.83fe0153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6387e82c3e15844f7d",
+    "url": "/coffee-simple-menu/static/js/main.bfa69af5.chunk.js"
+  },
+  {
+    "revision": "8660692274a8656d2869",
+    "url": "/coffee-simple-menu/static/js/runtime-main.a457e503.js"
+  }
+]);
